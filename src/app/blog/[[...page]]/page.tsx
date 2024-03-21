4c676aac0353c04c48c1459e2102cfc8a28185ee
MYCOMPUTER\安齋　祐樹 <mail@kuronokono.me>
2024-03-21 15:46:49 +0900

    とりあえずSSGで

--- src/app/blog/[[...page]]/page.tsx
+++ src/app/blog/[[...page]]/page.tsx
@@ -1,4 +1,20 @@
+import { glob } from "glob";
+
 import Blog from "@_/components/Blog";
+
+export const dynamic = "force-static";
+
+const max = 10;
+
+export async function generateStaticParams() {
+  const paths = await glob("**/src/app/blog/(article)/*", { ignore: "**/layout.tsx" });
+
+  const pages = Math.ceil(paths.length / max);
+
+  const pagesArray = [...Array(pages)].map((_, i) => i + 1);
+
+  return [{ page: [] }, pagesArray.map((number) => ({ page: [String(number)] }))];
+}
 
 type Props = {
   params: {
@@ -8,6 +24,6 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   return (
-    <Blog max={1} pagination page={params?.page ? Number(params.page) : undefined} />
+    <Blog max={max} pagination page={params?.page ? Number(params.page) : undefined} />
   );
 }
